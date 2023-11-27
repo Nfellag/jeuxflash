8c2080e95047220cfd3199c4deb47a3ad20c6710
Doudou210 <siakakoudou.doumbia@yahoo.com>
2023-11-27 22:19:18 +0100

    scoring total

---
+++ gameflash/frontend/pages/LoginPages.php
@@ -0,0 +1,91 @@
+<?php
+
+require "../../backend/bd.php";
+
+if (isset($_POST['SingUp'])) {
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    $requete = $bdd->prepare('INSERT INTO utilisateurs (email, username, password) VALUES ( :email, :username, :password)');
+    if ($requete->execute([
+        "email" => $email,
+        "username" => $username,
+        "password" => $hashedPassword,
+    ])) {
+        echo "Inscription réussie. Vous pouvez maintenant vous connecter.";
+    } else {
+        echo "L'inscription a échoué. Veuillez réessayer.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LoginPages</title>
+    <link rel="stylesheet" href="../css/LoginPages.css">
+    <link rel="stylesheet" href="../assets/bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <script src="../assets/bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="formSign">
+        <!-- <div class="row w-50"> -->
+
+            <div class="p-3 bg-white w-50 d-flex vh-100 justify-content-center align-items-center">
+            <!-- <div class="col"> -->
+                <div id="createPara" class="createPara" >
+                    <h1>FlashGame</h1>
+                    <h2>Bonjour, <br> et bienvenue sur FashGame</h2>
+                    <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+                    <button id="create" class="btn btn-danger border w-100">Create Account</button>
+                </div>
+                <form action="" method="POST" id="sign" class="sign">
+                    <h1 style=>Sign Up</h1>
+                    <hr />
+                    <div class="mb-3">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" placeholder="Username" name="username" class="form-control"/>
+                    </div>
+                    <div class="mb-3">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" placeholder="Email" name="email" class="form-control"/>
+                    </div>
+                    <div class="mb-3">
+                        <label htmlFor="password">Mot de passe</label>
+                        <input type="password" placeholder="Password" name="password" class="form-control"/>
+                    </div>
+                    <button class="btn btn-success w-100" type="submit" name="SingUp">Sign Up</button>
+                </form>
+            </div>
+            <div class="p-3 w-50 d-flex vh-100 justify-content-center align-items-center">
+            <!-- <div class="col"> -->
+                <div id="logPara" class="logPara no_activedPara">
+                    <h1>FlashGame</h1>
+                    <h2>Bonjour, <br> et bienvenue sur FashGame</h2>
+                    <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+                    <button id="login" class=" btn-default bg-light border w-100">Connexion</button>
+                </div>
+                <form action="../../backend/Password.php" method="POST" id="log" class="log active" >
+                    <h1>Login</h1>
+                    <hr />
+                    <div class="mb-3">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" placeholder="Username" name="username" class="form-control"/>
+                    </div>
+                    <div class="mb-3">
+                        <label htmlFor="">Mot de passe</label>
+                        <input type="password" placeholder="Password" name="password" class="form-control"/>
+                    </div>
+                    <button class="btn btn-success w-100" type="submit" name="logIn">Log In</button>
+                    <p> <a href="./reipassword.php"> Vous avez oublié votre mot de passe ?</a></p>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="../js/LoginPages.js"></script>
+</body>
+</html>
---
+++ gameflash/frontend/pages/reipassword.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Réinitialisation du mot de passe</title>
+    <link rel="stylesheet" href="./css/reipassword.css">
+</head>
+<body class="forgot_password_body" >
+
+    <div class="logo">
+        <img src="./img/Consisto-removebg-preview.png" alt="">
+    </div>
+
+    <div class="forgot_password_form">
+        <h2>Mot de passe oublié</h2>
+        <form action="" method="post">
+            <input type="email" placeholder="Adresse email *" name="email">
+            <button type="submit" name="send" >Send a mail</button>
+        </form>
+    </div>
+</body>
+</html>
+
+<?php
+
+// include "bd.php";
+
+// $to=$_POST['email'] ?? "test@mail.fr";
+// $subject = "réinitialisation de mot de passe";
+// $url ="http://localhost/Projet_Site_Location/subscribiton_part.php/token/index.php";
+// $message = "Le lien pour réinitialiser votre mo de passe est: $url";
+
+// $headers = "content-Type: text/plain; charset=utf-8\r\n";
+// $headers ="From: consisto185@gmail.com\r\n";
+
+// ini_set('SMTP', 'smtp.example.com');
+// ini_set('smtp_port', 587);
+
+// if (mail($to, $subject, $message, $headers)) {
+//     echo "Envoye !";
+// } else {
+//     echo "Erreur d'envoie !";
+// }
+
+include "../../backend/bd.php";
+
+if (isset($_POST['send'])) {
+    $email = $_POST["email"];
+    $token = uniqid();
+    $subject = "Réinitialisation du mot de passe";
+
+    $url = "http://localhost/jeuxflash/gameflash/backend/token/update_password.php?token=$token";
+
+    $message = "Bonjour, voici le lien pour réinitialiser votre mot de passe: $url";
+    $headers = "content-Type: text/plain; charset=utf-8\r\n";
+    $headers ="From: pro.test.projet@gmail.com\r\n";
+
+
+    if (mail($_POST['email'], $subject, $message, $headers)) {
+        $bd = "UPDATE utilisateurs SET token = ? WHERE email = ?";
+        $requete = $bdd->prepare($bd);
+        $requete -> execute([
+            $token,
+            $_POST['email'],
+        ]);
+        echo "Mail envoyé";
+    } else {
+        echo "Une erreur est survenue, veuillez ressayer";
+    }
+    
+}
